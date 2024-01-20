bcaba1266fe0a38b88747868c2f7579d9764b395
Murali Krishna Alakuntla <118282852+muralialakuntla3@users.noreply.github.com>
2024-01-20 12:04:24 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -8,5 +8,5 @@ output "subnet_id" {
   value = aws_subnet.lms_public_subnet.id
 }
 output "server_public_ip" {
-  value = aws_instance.ec2-server.public_ip
+  value = aws_instance.test-ec2.public_ip
 }
