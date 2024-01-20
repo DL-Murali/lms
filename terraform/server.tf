4bcb832ae316c79563833ba1e6051d8a8a09e549
Murali Krishna Alakuntla <118282852+muralialakuntla3@users.noreply.github.com>
2024-01-20 12:04:01 +0530

    Update server.tf

--- terraform/server.tf
+++ terraform/server.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "test-ec2" {
   ami           = "ami-0a7cf821b91bcccbc"
   instance_type = "t2.micro"
-  subnet_id = aws_subnet.lms_pub_sn.id
+  subnet_id = aws_subnet.lms_public_subnet.id
   key_name = "murali"
   vpc_security_group_ids = [aws_security_group.lms-sg.id]
 #  user_data = file("install_ansible.sh")  ---> if have any
