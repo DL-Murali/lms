6ce89ebf1732d326f99a775270053c9971ec2ff3
Murali Krishna Alakuntla <118282852+muralialakuntla3@users.noreply.github.com>
2024-01-20 12:05:00 +0530

    Create provider.tf

---
+++ terraform/provider.tf
@@ -0,0 +1,6 @@
+# AWS provider
+provider "aws" {
+  region     = "us-west-1"
+  access_key = "***********"                        # your access key details
+  secret_key = "***************************"        # your secret key details
+}
